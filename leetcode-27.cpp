4666f70ed5faa7555cfbfb650918bbe1b19d9f9b
breastcover <39230048+breastcover@users.noreply.github.com>
2018-08-11 10:55:16 +0800

    day three two

---
+++ leetcode-27.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        for(auto iter=nums.begin();iter!=nums.end();)
+        {
+            if(*iter==val)
+            {
+                nums.erase(iter);
+            }
+            else
+                iter++;
+        }
+        return nums.size();
+    }
+};
